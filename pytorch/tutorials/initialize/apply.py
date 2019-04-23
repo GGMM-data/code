6c91f815d07d6d6a9ec17f31830cd932cfdaa2b2
mxxhcm <mxxhcm@gmail.com>
2019-04-23 18:34:13 +0800

    add pytorch initialize parameters

---
+++ pytorch/tutorials/initialize/apply.py
@@ -0,0 +1,11 @@
+import torch.nn as nn
+
+def init_weights(m):
+  print(m)
+  if type(m) == nn.Linear:
+    m.weight.data.fill_(1.0)
+    print(m.weight)
+
+net = nn.Sequential(nn.Linear(2, 2), nn.Linear(2, 2))
+net.apply(init_weights)
+
