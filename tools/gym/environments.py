f3a76c1ef604beb297bf8bac5ad09bf5479155f7
mxxhcm <mxxhcm@gmail.com>
2019-04-28 19:58:40 +0800

    update blog

--- tools/gym/environments.py
+++ tools/gym/environments.py
@@ -3,12 +3,13 @@ import numpy as np
 import time
 
 env = gym.make("PongDeterministic-v4")
-env.reset()
+state = env.reset()
+print(type(state))
 while True:
     state, reward, done, _ = env.step(np.random.randint(env.action_space.n))
     env.render()
     if done:
-       print("Done")
-       time.sleep(3)
-       env.reset()
-    
+        print("Done")
+        time.sleep(3)
+        env.reset()
+
