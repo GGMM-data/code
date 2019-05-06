b1251f3d0f0bd75d3393d84b62dee27ac48b4c2e
mxxhcm <mxxhcm@gmail.com>
2019-05-07 00:52:43 +0800

    update code

---
+++ shell/install.sh
@@ -0,0 +1,15 @@
+sudo apt install -y make vim gcc g++
+sudo apt update
+sudo apt -y upgrade
+sudo apt install  -y python-pip python3-pip
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pip -U
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple matplotlib gym gym[atari] tensorflow==1.8.0 tqdm
+
+sudo apt install -y git fcitx ssh net-tools gparted shadowsocks
+sudo apt install -y nodejs npm
+sudo npm install -g hexo-cli
+
+sudo apt update
+sudo apt -y upgrade
+
