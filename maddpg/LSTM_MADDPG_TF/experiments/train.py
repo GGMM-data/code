6877a6c1d82be160bac187e78d3754741fe44cf7
mxxhcm <mxxhcm@gmail.com>
2019-07-07 15:35:36 +0800

    update code

--- maddpg/LSTM_MADDPG_TF/experiments/train.py
+++ maddpg/LSTM_MADDPG_TF/experiments/train.py
@@ -270,7 +270,7 @@ def train(arglist):
 					temp_efficiency = np.array(aver_cover_one_episode[task_index]) * np.array(
 						j_index_one_episode[task_index]) / np.array(energy_one_episode[task_index])
 					draw_util.draw_single_episode(
-						arglist.pictures_dir_train + model_name + "/single_episode_task_" + str(task_index) + "/",
+						arglist.pictures_dir_train + model_name + "single_episode_task_" + str(task_index) + "/",
 						episode_number,
 						temp_efficiency,
 						aver_cover_one_episode[task_index],
