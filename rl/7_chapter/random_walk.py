a21b4d54059a9809da7199ebfbafbf79cb25a7df
mxxhcm <mxxhcm@gmail.com>
2019-07-30 20:20:48 +0800

    update code

--- rl/7_chapter/random_walk.py
+++ rl/7_chapter/random_walk.py
@@ -6,16 +6,20 @@ class N_step_TD(object):
     def __init__(self):
         self.states = np.arange(21)
         self.ACTIONS = [-1, 1]
-        self.q_values = np.ones((len(self.states), len(self.ACTIONS)))
         
-    def learn(self, n, alpha, q)
+    def learn(self, n, alpha, q):
+        
 
-    def figure_7_2(self):
+    def figure_7_2(self, n, alpha):
         runs = 100
         episodes = 10
+        q_values = np.zeros((len(self.states), len(self.ACTIONS)))
         for r in range(runs):
+            current_q = np.copy(q_values)
             for e in range(episodes):
-                self.lZZe
+                self.learn(r, n, alpha, current_q)
+            
+            
 
 if __name__ == "__main__":
     
