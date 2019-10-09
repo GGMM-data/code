cecb0f390fb9ce52bc2b9e66957dc13b6684db80
mxxhcm <mxxhcm@gmail.com>
2019-10-09 21:28:22 +0800

    update code

---
+++ tools/py_gym/cartpole_random_monitor.py
@@ -0,0 +1,26 @@
+import gym
+import time
+
+if __name__ == "__main__":
+    env = gym.make("CartPole-v0")
+    env = gym.wrappers.Monitor(env, "recording")
+
+    total_reward = 0.0
+    total_steps = 0
+    obs = env.reset()
+    episode = 1
+    while True:
+        action = env.action_space.sample()
+        obs, reward, done, _ = env.step(action)
+        total_reward += reward
+        total_steps += 1
+        #env.render()
+        if done:
+            print("Episode %d done in %d steps, total reward %.2f" %(episode, total_steps, total_reward))
+            time.sleep(1)
+            env.reset()
+            if episode > 5:
+                break
+            episode += 1
+            total_reward = 0
+
--- tools/py_gym/random_action_wrapper.py
+++ tools/py_gym/random_action_wrapper.py
@@ -1,32 +1,46 @@
 import gym
 import random
+import time
+
 
 class RandomActionWrapper(gym.ActionWrapper):
-    def __int__(self, env, epsilon=0.1):
-        super(RandomActionWrapper, self).__init__(env)
+    def __init__(self, environment, epsilon=-1.1):
+        super(RandomActionWrapper, self).__init__(environment)
         self.epsilon = epsilon
 
     def action(self, action):
         if random.random() < self.epsilon:
             print("Random")
             return self.env.action_space.sample()
+            # return self.environment.action_space.sample()
+        else:
+            # print("Not random")
+            pass
         return action
 
+    def reverse_action(self, action):
+        pass
+
+
 if __name__ == "__main__":
-    env = gym.make("CartPole-v0")
-    env = RandomActionWrapper(env, epsilon=0.3)
+    environment = gym.make("CartPole-v0")
+    env = RandomActionWrapper(environment)
     
     obs = env.reset()
-    episode = 1
+    episode = 0
+    total_steps = 0
+    total_reward = 0
     while True:
         action = env.action_space.sample()
-        obs, reward, done, _ = env.step(action)
+        obs, reward, done, info = env.step(action)
+        print(reward)
         total_reward += reward
         total_steps += 1
         env.render()
+        time.sleep(0.1)
         if done:
-            print("Episode %d done in %d steps, total reward %.2f" %(episode, total_steps, total_reward))
-            time.sleep(1)
+            print("Episode %d done in %d steps, total reward %.1f" %(episode, total_steps, total_reward))
+            time.sleep(0.1)
             env.reset()
             episode += 1
             total_reward = 0
